8b286d531d5d0daac3c39134bac772abdfa6d62c
gnuhpc <gnuhpc@gmail.com>
2016-06-22 17:19:16 +0800

    Add Search Range Limit

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var proxy = hoxy.createServer({
     reverse: 'http://192.168.145.128:5601',
 });
 
+var _MS_PER_DAY = 1000 * 60 * 60 * 24;
+// a and b are javascript Date objects
+function dateDiffInDays(a, b) {
+    // Discard the time and time-zone information.
+    var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
 proxy.intercept({
     phase: 'request',
     method: 'POST',
@@ -19,10 +29,26 @@ proxy.intercept({
         indexname;
 
     if (requrl.indexOf('_msearch') != -1) {
+        reqparams = req.string.split('\n');
+        indexname = JSON.parse(reqparams[0])['index'];
+        post_time = JSON.parse(reqparams[1])['query']['filtered']['filter']['bool']['must'][0]['range']['post_date'];
+        starttime =  post_time.gte;
+        endtime =  post_time.lte;
         console.log('request made to: ' + requrl);
-        console.log('request header:' + requser);
-        indexname = JSON.parse(req.string.split('\n')[0]).index[0];
-        console.log('indexname name:' + indexname);
+        console.log("Request String is : " + req.string);
+        console.log("Start Time :" + new Date(starttime) + ',' + starttime);
+        console.log("End Time :" + new Date(endtime)+','+endtime);
+        console.log("Time Diff in days:" + Math.floor((endtime-starttime)/_MS_PER_DAY));
+        if(Math.floor((endtime-starttime)/_MS_PER_DAY)>7){
+            console.log("Search Range is greater than 7 days, Change its start:" + (starttime+8*_MS_PER_DAY).toString());
+            req.string = req.string.replace(starttime.toString(),(starttime+8*_MS_PER_DAY).toString());
+            console.log("Request String has been modified: " + req.string);
+        }
+        if (indexname instanceof  Array) {
+            indexname = indexname[0]
+        }
+         console.log('index name is :' + indexname);
+         console.log('request users:' + requser);
         if (indexname.indexOf(requser) == -1){
             req.string = req.string.replace(indexname,'null');
             console.log(indexname+ ' has been changed to null');

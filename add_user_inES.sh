fb4fcd1ecbaa3c4d735077cc6c942f1c7c129ae9
Pengcheng Huang <futuredaemon@gmail.com>
2019-05-06 14:50:38 +0800

    Update add_user_inES.sh

--- add_user_inES.sh
+++ add_user_inES.sh
@@ -11,7 +11,7 @@ htpasswd2 -b /logger/nginx/conf/.kibanahtpasswd $1 $2
 pscp -r -v -l logger -t 0 -h ~/client_server /logger/nginx/conf/ /logger/nginx/.
  
 str1='curl -XPUT '
-str2=\'"http://localhost:9201/.cmbc/user_info/$1_$3"\'
+str2=\'"http://localhost:9201/.user/user_info/$1_$3"\'
 str3=' -d '
 str4=\'"{ \"user\" : \"$1\", \"indexname\" : \"$3\"}"\'
  
